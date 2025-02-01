1e7b77ac609a62151fa82513cc88792662385893
Raja Arbab <rajaarbab745@gmail.com>
2025-02-01 23:35:30 +0500

    js work

---
+++ script.js
@@ -0,0 +1,10 @@
+                                //   Alerts
+// Question:01
+alert ("welcome to Raja'$ assigment!");
+//Question:02
+alert("Error! Please enter a valid password");
+//Question:03
+alert("Welcome to First JS Assignment \n Raja'$ coding!");
+
+console.log ("Hello This Is a Developer Console" );
+
