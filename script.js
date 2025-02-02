3b1e32af51ee6372eb9d66195583c0a2ba2c9ee7
Raja Arbab <rajaarbab745@gmail.com>
2025-02-02 12:44:06 +0500

    some more work

--- script.js
+++ script.js
@@ -1,10 +1,61 @@
-                                //   Alerts
+//   Alerts
 // Question:01
-alert ("welcome to Raja'$ assigment!");
+alert("welcome to Raja'$ assigment!");
+
 //Question:02
 alert("Error! Please enter a valid password");
+
 //Question:03
 alert("Welcome to First JS Assignment \n Raja'$ coding!");
 
-console.log ("Hello This Is a Developer Console" );
+//Question:05
+console.log("Hello This Is a Developer Console");
+
+//Variable For Strings
+//Qustion :01
+var userName = "";
+
+//Question:02
+var myName = "Raja Arbab";
+
+//Question:03
+//a)
+var message;
+//b)
+message = "Hello World"
+//c)
+alert(message)
+
+//Question:04
+var studentName = "Wasil Tanoli";
+var age = "19 years old";
+var course = "Web & Mobile App Development";
+var institute = "SMIT (Saylani Mass IT Training);";
+
+alert(studentName);
+alert(age);
+alert(course);
+alert(institute);
+
+//Question:05
+var message = "pizza\npizz\npiz\npi\np";
+alert(message);
+
+//Question:.06
+var email = "rajaarbab115@gmail.com";
+alert("My email address is " + email);
+
+//Question:07
+var book = " A smarter \n way to learn JavaScript";
+alert("I am trying to learn from the book" + book);
+
+//Question:08
+var script = "Yeh! I can write HTML content through java script";
+document.write(script);
+
+//Question:09
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(design);
+
+
 
